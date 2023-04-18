ee513bcd44cdd7b1c3123eb81987290300ac390f
RambiyAnzur <125882864+RambiyAnzur@users.noreply.github.com>
2023-04-18 12:51:34 +0300

    Update flower_quiz.py

--- flower_quiz.py
+++ flower_quiz.py
@@ -1,1 +1,29 @@
-flower_quiz.py
+
+# Код программы Flower Quiz
+
+import random
+
+flowers = ['розы', 'тюльпаны', 'орхидеи', 'лилии', 'ирисы']
+descriptions = {
+    'розы': 'Цветок, символизирующий любовь и страсть',
+    'тюльпаны': 'Цветок, символизирующий уважение и понимание',
+    'орхидеи': 'Цветок, символизирующий красоту и изысканность',
+    'лилии': 'Цветок, символизирующий чистоту и нежность',
+    'ирисы': 'Цветок, символизирующий мудрость и верность'
+}
+
+def get_random_flower():
+    # функция для получения случайного цветка и его описания
+    flower = random.choice(flowers)
+    description = descriptions[flower]
+    return flower, description
+
+flower, description = get_random_flower()
+
+print(f"Угадайте, к какому виду цветка относится представленный цветок: {flower}")
+answer = input()
+
+if answer == flower:
+    print("Правильно!")
+else:
+    print(f"Неверно. Правильный ответ: {flower}. {description}")
